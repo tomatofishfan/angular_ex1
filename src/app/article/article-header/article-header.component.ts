1be07498ed78d44f200635f3cbb79e0f2d957e48
tomato <tomatofishfan@gmail.com>
2019-10-12 14:25:42 +0800

    課程閱讀第一遍

--- src/app/article/article-header/article-header.component.ts
+++ src/app/article/article-header/article-header.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, Input, Output, EventEmitter, OnChanges } from '@angu
 @Component({
   selector: 'app-article-header',
   templateUrl: './article-header.component.html',
-  styleUrls: ['./article-header.component.css']
+  styleUrls: ['./article-header.component.css'],
 })
 export class ArticleHeaderComponent implements OnInit, OnChanges {
   @Input()
@@ -19,6 +19,7 @@ export class ArticleHeaderComponent implements OnInit, OnChanges {
 
   isEdit = false;
   newTitle = '';
+  orig_item;
 
   constructor() { }
 
@@ -27,18 +28,26 @@ export class ArticleHeaderComponent implements OnInit, OnChanges {
   }
 
   doEdit(title) {
-    this.newTitle = title;
-    this.titleChange.emit({ id: this.item.id, title: title });
+    // this.newTitle = title;
+    // this.titleChange.emit({ id: this.item.id, title: title });
+    this.titleChange.emit(this.item);
   }
 
   ngOnInit() {
-    this.newTitle = this.item.title;
+    // this.newTitle = this.item.title;
   }
 
   ngOnChanges(changes) {
     if (changes.item) {
+      this.orig_item = changes.item.currentValue;
       this.item = Object.assign({}, changes.item.currentValue);
+      // 讓父子 item 脫鉤，而在 html 改採雙向繫結
     }
+  }
+
+  doCancel() {
+    this.item = Object.assign({}, this.orig_item);
+    this.isEdit = false;
   }
 
 }
--- src/app/article/article.module.ts
+++ src/app/article/article.module.ts
@@ -3,11 +3,17 @@ import { CommonModule } from '@angular/common';
 import { ArticleListComponent } from './article-list/article-list.component';
 import { ArticleHeaderComponent } from './article-header/article-header.component';
 import { ArticleBodyComponent } from './article-body/article-body.component';
+import { FormsModule } from '@angular/forms';
+import { DataService } from './data.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    HttpClientModule,
   ],
+  providers: [DataService],
   declarations: [ArticleListComponent, ArticleHeaderComponent, ArticleBodyComponent],
   // 需要匯出才能在 AppModule 使用
   exports: [ArticleListComponent]
